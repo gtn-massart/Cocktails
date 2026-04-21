2b139ff8e5c9e8ec544f03c867e3dd255b38c9d4
Gaëtan Massart <gtn.massart@gmail.com>
2026-04-21 14:46:45 +0200

    add mojito

--- src/app/components/cocktails/components/cocktail-details/cocktail-details.ts
+++ src/app/components/cocktails/components/cocktail-details/cocktail-details.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   templateUrl: './cocktail-details.html',
   styleUrl: './cocktail-details.scss',
 })
-export class CocktailDetails {}
+export class CocktailDetails {
+  cocktail: Cocktail = {
+    imageUrl:
+      'https://images.unsplash.com/photo-1622322331040-088df8424c89?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Mojito',
+    description: `Le mojito est un cocktail classique d'origine cubaine, très rafraîchissant et populaire dans le monde entier.`,
+  };
+}
