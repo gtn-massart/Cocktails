a075e04706416cff1a2a2b52bc8256f310ef48e4
Gaëtan Massart <gtn.massart@gmail.com>
2026-04-21 08:40:45 +0200

    setup cocktail

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Cocktails } from './components/cocktails/cocktails';
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, Cocktails ,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
