c8f908d6cf22327914656bb9e6baf724f9bc54b3
Gaëtan Massart <gtn.massart@gmail.com>
2026-04-21 10:25:08 +0200

    layout cocktails

--- src/app/components/cocktails/cocktails.ts
+++ src/app/components/cocktails/cocktails.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CocktailsList } from './components/cocktails-list/cocktails-list';
+import { CocktailDetails } from './components/cocktail-details/cocktail-details';
 
 @Component({
   selector: 'app-cocktails',
-  imports: [],
+  imports: [CocktailsList, CocktailDetails],
   templateUrl: './cocktails.html',
   styleUrl: './cocktails.scss',
 })
