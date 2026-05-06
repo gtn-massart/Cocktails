42fd51f280cedca019c6b037d4a30513773647e5
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-06 11:06:05 +0200

    erease useless import

--- src/app/shared/data/cocktails.data.ts
+++ src/app/shared/data/cocktails.data.ts
@@ -1,7 +1,5 @@
-import { Cocktail } from '../interfaces/cocktail.interface';
-
 export const cocktails = [
-    {
+  {
     imageUrl:
       'https://static.750g.com/images/1200-675/dfe52623942a8b2e6b4f1e1715a42570/servez-et-degustez.jpg',
     name: 'Mojito',
@@ -20,8 +18,7 @@ export const cocktails = [
     imageUrl:
       'https://assets.bonappetit.com/photos/5b69f16006027f654a27cd19/3:2/w_3693,h_2462,c_limit/ba-margarita-1.jpg',
     name: 'Margarita',
-    description:
-      'Un cocktail mexicain classique à base de tequila, citron vert et triple sec.',
+    description: 'Un cocktail mexicain classique à base de tequila, citron vert et triple sec.',
     ingredients: [
       '50ml de tequila',
       '25ml de jus de citron vert',
@@ -34,8 +31,7 @@ export const cocktails = [
     imageUrl:
       'https://assets.epicurious.com/photos/579a2d8e437fcffe02f7230b/master/pass/caipirinha-072816.jpg',
     name: 'Caipirinha',
-    description:
-      'Un cocktail brésilien frais et acidulé à base de cachaça et citron vert.',
+    description: 'Un cocktail brésilien frais et acidulé à base de cachaça et citron vert.',
     ingredients: [
       '50ml de cachaça',
       '1 citron vert coupé en quartiers',
@@ -47,8 +43,7 @@ export const cocktails = [
     imageUrl:
       'https://assets.epicurious.com/photos/623e1de1b623bb3c6f80a625/4:3/w_4960,h_3720,c_limit/Not-a-Colada_HERO_RECIPE_032422_30290.jpg',
     name: 'Piña Colada',
-    description:
-      "Un cocktail tropical sucré à base d'ananas, noix de coco et rhum.",
+    description: "Un cocktail tropical sucré à base d'ananas, noix de coco et rhum.",
     ingredients: [
       '50ml de rhum blanc',
       "100ml de jus d'ananas",
@@ -60,8 +55,7 @@ export const cocktails = [
     imageUrl:
       'https://www.1001cocktails.com/wp-content/uploads/1001cocktails/2023/03/80274_origin-scaled-2048x1366.jpg',
     name: 'Cosmopolitan',
-    description:
-      'Un cocktail sophistiqué et fruité à base de vodka, cranberry et citron.',
+    description: 'Un cocktail sophistiqué et fruité à base de vodka, cranberry et citron.',
     ingredients: [
       '40ml de vodka',
       '15ml de triple sec',
@@ -75,12 +69,6 @@ export const cocktails = [
       'https://res.cloudinary.com/htt8g4cd/images/w_1280,h_853,c_scale/f_auto,q_auto/v1730840944/wp/11_22_Dirty_Vodka_Martini_HERO_Ali_Redmond_1920x1280/11_22_Dirty_Vodka_Martini_HERO_Ali_Redmond_1920x1280.jpg?_i=AA',
     name: 'Martini',
     description: 'Un cocktail élégant à base de gin et vermouth sec.',
-    ingredients: [
-      '60ml de gin',
-      '10ml de vermouth sec',
-      'Zeste de citron ou olive',
-      'Glaçons',
-    ],
+    ingredients: ['60ml de gin', '10ml de vermouth sec', 'Zeste de citron ou olive', 'Glaçons'],
   },
 ];
-
