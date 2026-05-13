17ecd67b091243ee6f65e215bed3f6687f8b3d85
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-13 11:50:48 +0200

    display formulary

--- src/app/views/admin/views/admin-cocktails/views/admin-cocktails-form/admin-cocktails-form.ts
+++ src/app/views/admin/views/admin-cocktails/views/admin-cocktails-form/admin-cocktails-form.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-cocktails-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './admin-cocktails-form.html',
   styleUrl: './admin-cocktails-form.scss',
+  host: { class: 'card' },
 })
-export class AdminCocktailsForm {}
+export class AdminCocktailsForm {
+  private fb = inject(FormBuilder);
+
+  cocktailForm = this.fb.group({
+    name: ['', Validators.required],
+    imageUrl: [''],
+    description: [''],
+    ingredients: this.fb.array([]),
+  });
+
+  get ingredientsControl() {
+    return this.cocktailForm.get('ingredients') as FormArray;
+  }
+
+  get nameControl() {
+    return this.cocktailForm.get('name') as FormControl;
+  }
+
+  addIngredient() {
+    this.ingredientsControl.push(this.fb.control(''));
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredientsControl.removeAt(index);
+  }
+
+  submit() {}
+}
