cde0120a680ad0c7abff3de88b51fb5bd0cc33b8
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-13 07:50:58 +0200

    modif BASE_URL

--- src/app/shared/data/seed.ts
+++ src/app/shared/data/seed.ts
@@ -1,7 +1,7 @@
 import { cocktails } from './cocktails.data';
 
 export async function seedData() {
-  await fetch('https://restapi.fr/api/acocktails', {
+  await fetch('https://restapi.fr/api/cocktails', {
     method: 'POST',
     body: JSON.stringify(cocktails),
     headers: {
