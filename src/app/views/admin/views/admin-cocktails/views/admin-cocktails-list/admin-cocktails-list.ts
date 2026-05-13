f10bcb948c1762a6ca1bab4bd0ced1c94a0bee3e
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-13 07:24:30 +0200

    display admin-cocktaills-list

--- src/app/views/admin/views/admin-cocktails/views/admin-cocktails-list/admin-cocktails-list.ts
+++ src/app/views/admin/views/admin-cocktails/views/admin-cocktails-list/admin-cocktails-list.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, computed, Host, inject } from '@angular/core';
+import { CocktailsService } from '../../../../../../shared/services/cocktails.service';
 
 @Component({
   selector: 'app-admin-cocktails-list',
   imports: [],
   templateUrl: './admin-cocktails-list.html',
   styleUrl: './admin-cocktails-list.scss',
+  host: {class: 'card'}
 })
-export class AdminCocktailsList {}
+export class AdminCocktailsList {
+  cocktailsService = inject(CocktailsService);
+  cocktails = computed(() => this.cocktailsService.cocktailsResource.value() || []);
+}
