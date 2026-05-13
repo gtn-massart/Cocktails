94dafdf8488d8a81c9e71897cfd8a2c4731195e3
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-13 07:50:38 +0200

    add service deleteCocktail(id)

--- src/app/shared/services/cocktails.service.ts
+++ src/app/shared/services/cocktails.service.ts
@@ -1,13 +1,20 @@
 import { Injectable, resource } from '@angular/core';
 import { Cocktail } from '../interfaces/cocktail.interface';
 
+const BASE_URL = 'https://restapi.fr/api/cocktails';
+
 @Injectable({
   providedIn: 'root',
 })
 export class CocktailsService {
-  BASE_URL = 'https://restapi.fr/api/acocktails';
-
   cocktailsResource = resource({
-    loader: async (): Promise<Cocktail[]> => (await fetch(this.BASE_URL)).json(),
+    loader: async (): Promise<Cocktail[]> => (await fetch(BASE_URL)).json(),
   });
+
+  async deleteCocktail(cocktailId: string) {
+    await fetch(`${BASE_URL}/${cocktailId}`, {
+      method: 'DELETE',
+    });
+    this.cocktailsResource.reload();
+  }
 }
--- src/app/views/admin/views/admin-cocktails/views/admin-cocktails-list/admin-cocktails-list.ts
+++ src/app/views/admin/views/admin-cocktails/views/admin-cocktails-list/admin-cocktails-list.ts
@@ -1,14 +1,19 @@
 import { Component, computed, Host, inject } from '@angular/core';
 import { CocktailsService } from '../../../../../../shared/services/cocktails.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin-cocktails-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './admin-cocktails-list.html',
   styleUrl: './admin-cocktails-list.scss',
-  host: {class: 'card'}
+  host: { class: 'card' },
 })
 export class AdminCocktailsList {
   cocktailsService = inject(CocktailsService);
   cocktails = computed(() => this.cocktailsService.cocktailsResource.value() || []);
+
+  deleteCocktail(cocktailId: string) {
+    this.cocktailsService.deleteCocktail(cocktailId);
+  }
 }
