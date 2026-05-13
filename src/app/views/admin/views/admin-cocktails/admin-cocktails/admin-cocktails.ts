ae403643fc5a3b9800b3fe637ff6fcf39befa456
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-13 07:03:16 +0200

    add feaure in admin

--- src/app/views/admin/views/admin-cocktails/admin-cocktails/admin-cocktails.ts
+++ src/app/views/admin/views/admin-cocktails/admin-cocktails/admin-cocktails.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-cocktails',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './admin-cocktails.html',
   styleUrl: './admin-cocktails.scss',
 })
